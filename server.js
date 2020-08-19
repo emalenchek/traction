07e691e7755c291f534b5fb1bfc2a2de058566e9
Ethan Malenchek <emalenchek@gmail.com>
2020-08-19 04:52:20 -0400

    configured server.js and dev scripts

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express"); //backend framework
+const mongoose = require('mongoose'); //mongoDB ORM
+const path = require('path');
+const config = require('config');
+
+require("dotenv").config();
+
+const app = express();
+
+// Bodyparser middleware
+
+app.use(express.json());
+
+// MongoDB URI DB config
+
+const db = config.get('mongoURI');
+
+// connect to Mongo
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log('MongoDB connected ...'))
+    .catch(err => console.log(err));
+
+// Use routes
+
+//
+//
+//
+//
+
+
+// Serve static assets if in production
+
+if(process.env.NODE_ENV === 'production') {
+    // Set static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+// environmental variable / Port 5000
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
